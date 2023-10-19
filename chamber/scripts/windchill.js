afd5a45cea3cf580e2e24e9e1fd803185dee2175
spencerengmann <spencerengmann@gmail.com>
2023-10-19 17:29:46 -0600

    updates

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,19 @@
+function showwindchill(temp, windspeed) {
+    const windchillSpan = document.getElementById('windchill');
+    let message="N/A";
+    if(temp<=50 && speed >3){
+        let chillfactor= Math.pow(speed, 0.16);
+        let chill = Math.round(35.74 + (0.6215 *temp) - (35.75*chillfactor) + (0.4275* temp * chillfactor));
+        message = "$(chill)";
+    }
+
+    windchillSpan.textContent = message;
+
+}
+
+const temperatureSpan = document.getElementById('temperature');
+const windspeedSpan = document.getElementById('windspeed');
+const temperature = parseInt(temperatureSpan.textContent);
+const windspeed = parseInt(windspeedSpan.textContent);
+
+showwindchill(temperature, windspeed);
